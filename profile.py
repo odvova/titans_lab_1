d04f245bda6b69ff7595928d70c4d069d03e262c
odvova <dovakin746@gmail.com>
2023-04-01 10:44:00 +0300

    HW#1 tasks 1-3 refactored

--- profile.py
+++ profile.py
@@ -1,13 +1,5 @@
-
 class Profile:
-    def __init__(self, name,
-                 last_name,
-                 phone_number,
-                 address,
-                 email,
-                 birthday,
-                 age,
-                 sex):
+    def __init__(self, name, last_name, phone_number, address, email, birthday, age, sex):
         self.name = name
         self.last_name = last_name
         self.phone_number = phone_number
@@ -18,14 +10,11 @@ class Profile:
         self.sex = sex
 
     def __str__(self):
-        return f" Name: {self.name}\n " \
-               f"Last Name: {self.last_name}\n " \
-               f"Phone Number: {self.phone_number}\n " \
-               f"Address: {self.address}\n " \
-               f"Email: {self.email}\n " \
-               f"Birthday: {self.birthday}\n " \
-               f"Age: {self.age}\n " \
-               f"Sex: {self.sex}"
-
-
-
+        return (f"Name: {self.name}\n"
+                f"Last Name: {self.last_name}\n"
+                f"Phone Number: {self.phone_number}\n"
+                f"Address: {self.address}\n"
+                f"Email: {self.email}\n"
+                f"Birthday: {self.birthday}\n"
+                f"Age: {self.age}\n"
+                f"Sex: {self.sex}")
